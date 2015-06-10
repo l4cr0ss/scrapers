fb0e27a424b996c051011c1e59e47692203e3919
jefferson.hudson@gmail.com <jefferson@home_devel.attlocal.net>
2015-06-10 16:06:23 -0400

    Test

--- niagara_4248/connect.pl
+++ niagara_4248/connect.pl
@@ -1,3 +1,11 @@
 #!/usr/bin/perl
 #
 # Connect to the Niagra 4248 via telnet
+
+use strict;
+use warnings;
+
+# Import libraries
+use Config::Simple;
+
+Config::Simple->import_from('app.ini', \%Config);
