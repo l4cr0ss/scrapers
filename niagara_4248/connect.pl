1aa73891323fe42072132ed44989108ca37fe880
Jefferson Hudson <jefferson.hudson@gmail.com>
2015-06-10 17:08:49 -0400

    Added code to read in a 3rd line from the .ini file and store it in a variable called . This is needed by the development branch to succesfully connect to the host via telnet.

--- niagara_4248/connect.pl
+++ niagara_4248/connect.pl
@@ -5,29 +5,32 @@
 use strict;
 use warnings;
 use Net::Telnet();
+use Switch;
 my $filename = 'app.ini';
 open(my $fh, '<:encoding(UTF-8)', $filename)
   or die "Could not open file '$filename' $!";
 
-my $l1 = 1;
 my $user = "";
 my $pass = "";
-for (my $i=0; $i<2; $i++) {
+my $host = "";
+my $i=0;
+for ($i=0; $i<3; $i++) {
   my $row = <$fh>;
   chomp $row;
-  if ($l1 == 1) {
-    $user = $row;
-    $l1 = 0;
-  } else {
-    $pass = $row; 
+  switch ($i) {
+    case 0  { $user = $row; }
+    case 1  { $pass = $row; }
+    case 2  { $host = $row; }
   }
 }
 
 print "$user\n";
 print "$pass\n";
+print "$host\n";
 
-$t = new Net::Telnet (Timeout => 10, Prompt => '/bash\$ $/');
-$t->open($host);
-$t->login($user, $pass);
-@lines = $t->cmd("show system info");
-print @lines;
+
+#$t = new Net::Telnet (Timeout => 10, Prompt => '/bash\$ $/');
+#$t->open($host);
+#$t->login($user, $pass);
+#@lines = $t->cmd("show system info");
+#print @lines;
