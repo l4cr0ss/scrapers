2936b7b73984ce553f28620681e25de61c1ae283
Jefferson <jefferson.hudson@bbb1.com>
2015-06-11 02:46:14 +0000

    Working on an experiment using the option callbacks feature of the Net::Telnet library in order to negotiate the terminal size presented to the remote server.

--- niagara_4248/connect.pl
+++ niagara_4248/connect.pl
@@ -24,17 +24,55 @@ for ($i=0; $i<3; $i++) {
   }
 }
 
-my ($t, @output);
-$t = new Net::Telnet (Timeout => 10, 
-                      Prompt => '/([\$%#>] $|--More--)/',
-                      Input_log => 'input.log', 
-                      Output_log => 'output.log',
-                      Dump_log => 'dump.log');
-$t->open($host);
-$t->login($user, $pass);
+my $prompt = '/[\$%#>] $/';
+
 
 #my @lines = $t->cmd("show system info");
 #print @lines;
 
-my @lines = $t->cmd("show port sfp");
+my @lines;
+my @lines2;
+my @lines3;
+
+$t->print("show port sfp");
+@lines = $t->waitfor("/--More--/");
+$t->print(" ");
+@lines2 = $t->waitfor("/--More--/");
+$t->print(" ");
+@lines3 = $t->waitfor($prompt);
 print @lines;
+print @lines2;
+print "@lines3\n";
+
+my $topts = Net::Telnet::Options->new();
+my $t;
+$t = new Net::Telnet (Timeout => 10, 
+                      Prompt => '/[\$%#>] $/',
+                      Input_log => 'input.log', 
+                      Output_log => 'output.log',
+                      Option_log => 'option.log',		
+                      Dump_log => 'dump.log');
+
+$t->option_callback(\&opt_callback);
+$t->option_accept([ Do => TELNET_NAWS ])
+$t->open($host);
+$t->login($user, $pass);
+sub opt_callback 
+{
+	my ($obj, $option, $is_remote, $is_enabled, $was_enabled, $buf_pos) = @_;
+	if ($option == TELNET_NAWS and $is_enabled and !$is_remote) {
+		$obj->put(TELNET_IAC,TELNET_SB,TELNET_NAWS,"\x03\xE8",TELNET_IAC,TELNET_SE);
+	}
+}
+
+
+    ## Indicate that we'll accept an offer from remote side for it to echo
+    ## and suppress go aheads.
+    &_opt_accept($self,
+		 { option    => &TELOPT_ECHO,
+		   is_remote => 1,
+		   is_enable => 1 },
+		 { option    => &TELOPT_SGA,
+		   is_remote => 1,
+		   is_enable => 1 },
+		 );
