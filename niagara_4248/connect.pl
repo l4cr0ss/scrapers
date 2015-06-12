6668a325560f412d7aac40c91df56fe47f61aedf
Jefferson <jefferson.hudson@bbb1.com>
2015-06-12 17:55:56 +0000

    experiment failure. trying the expect library.

--- niagara_4248/connect.pl
+++ niagara_4248/connect.pl
@@ -2,10 +2,14 @@
 #
 # Connect to the Niagra 4248 via telnet
 
-use strict;
+#use strict;
 use warnings;
-use Net::Telnet();
 use Switch;
+use Net::Telnet qw(TELOPT_NAWS);
+use Net::Telnet qw(TELNET_IAC);
+use Net::Telnet();
+
+# Load the configuration from the file app.ini
 my $filename = 'app.ini';
 open(my $fh, '<:encoding(UTF-8)', $filename)
   or die "Could not open file '$filename' $!";
@@ -24,55 +28,29 @@ for ($i=0; $i<3; $i++) {
   }
 }
 
+# Generic shell prompt
 my $prompt = '/[\$%#>] $/';
+my $more = '/--More--/';
+my $regex = "/$prompt||$more/";
+my $show_info = 'show system info';
 
 
-#my @lines = $t->cmd("show system info");
-#print @lines;
 
-my @lines;
-my @lines2;
-my @lines3;
-
-$t->print("show port sfp");
-@lines = $t->waitfor("/--More--/");
-$t->print(" ");
-@lines2 = $t->waitfor("/--More--/");
-$t->print(" ");
-@lines3 = $t->waitfor($prompt);
-print @lines;
-print @lines2;
-print "@lines3\n";
-
-my $topts = Net::Telnet::Options->new();
+# Telnet objects
 my $t;
-$t = new Net::Telnet (Timeout => 10, 
+
+$t = new Net::Telnet (Timeout => 10,
                       Prompt => '/[\$%#>] $/',
+		      Errmode => 'return',
                       Input_log => 'input.log', 
                       Output_log => 'output.log',
                       Option_log => 'option.log',		
                       Dump_log => 'dump.log');
 
-$t->option_callback(\&opt_callback);
-$t->option_accept([ Do => TELNET_NAWS ])
 $t->open($host);
 $t->login($user, $pass);
-sub opt_callback 
-{
-	my ($obj, $option, $is_remote, $is_enabled, $was_enabled, $buf_pos) = @_;
-	if ($option == TELNET_NAWS and $is_enabled and !$is_remote) {
-		$obj->put(TELNET_IAC,TELNET_SB,TELNET_NAWS,"\x03\xE8",TELNET_IAC,TELNET_SE);
-	}
-}
 
+$t->print("show port sfp");
+my @lines = $t->waitfor($more);
+print @lines;
 
-    ## Indicate that we'll accept an offer from remote side for it to echo
-    ## and suppress go aheads.
-    &_opt_accept($self,
-		 { option    => &TELOPT_ECHO,
-		   is_remote => 1,
-		   is_enable => 1 },
-		 { option    => &TELOPT_SGA,
-		   is_remote => 1,
-		   is_enable => 1 },
-		 );
